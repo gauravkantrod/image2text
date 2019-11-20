07510fc8af36d3c6f1ddc060e24f769e476ec1c7
GAK <gauravk1994@gmail.com>
2019-11-20 20:59:09 +0530

    Tried to add GUI for the image to text.

--- image2text.py
+++ image2text.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 
 app1 = Flask(__name__)
 
@@ -9,16 +9,23 @@ import os
 import re
 
 
-@app1.route("/appcheck", methods = ["GET"])
+@app1.route("/appcheck", methods=["GET"])
 def check_app_up():
     return "Application is running."
 
 
+@app1.route("/form", methods=["GET"])
+def from_input():
+    return render_template("index.html")
+
+
 @app1.route("/extract_date", methods=['POST'])
 def get_data():
-    file = request.files['file']
+    file = request.files['image']
+    print(file)
     text = pytesseract.image_to_string(Image.open(file).convert("L"), lang='eng')
     date = re.findall(r'\d{2}[./-]\d{2}[-./]\d{2,4}|\d{2,4}[-/]\w{3,4}[-/]\d{2,4}|\d{1,2}[/]\d{2,4}|\w{3,9}\s?\d{1,2}[\,]\s?\d{4}|\d{2}[/]\w{3,9}[/]\d{2,4}',text)
+
     if len(date) == 0:
         return jsonify(date='null')
     else:
